f1fa32661eb10162d8d129adbb00f3dd5f9f6ade
donaababu <70830550+donaababu@users.noreply.github.com>
2020-09-06 00:58:03 -0400

    Update settings.py
    Removed secret key

--- pasgen/settings.py
+++ pasgen/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '3=ugkzpd*887f#yjs9*)!k-0g6$3jj8tg(9^k=sa4oqo+=0c2&'
+SECRET_KEY = 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
